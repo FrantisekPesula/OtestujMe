584467ddb89295de0a41d3ae01dab9c5f667440f
František Pešula <frantiek.peula@student.gyarab.cz>
2025-01-13 10:56:04 +0100

    blablablabla

--- src/main/java/org/example/MyMin.java
+++ src/main/java/org/example/MyMin.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class MyMin {
     public static int min(int x, int y) {
-        return x < y ? x : x;
+        return x < y ? x : y;
     }
 
     public static void main(String[] argv) {
         Scanner sc = new Scanner(System.in);
+        var vysledek = 10;
+
         System.out.println("Nejmensi cislo ze vsech je: " + vysledek);
     }
 }
